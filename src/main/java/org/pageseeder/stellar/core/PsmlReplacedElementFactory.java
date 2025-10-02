40b79d311c1cbfc7ea7a8e89e1bf7d98441171ce
clauret <clauret@weborganic.com>
2025-10-03 09:24:56 +1000

    Bugfix: Ensure that images always fit on the page

--- src/main/java/org/pageseeder/stellar/core/PsmlReplacedElementFactory.java
+++ src/main/java/org/pageseeder/stellar/core/PsmlReplacedElementFactory.java
@@ -4,6 +4,10 @@ import com.lowagie.text.Image;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Element;
+import org.xhtmlrenderer.css.constants.CSSName;
+import org.xhtmlrenderer.css.newmatch.CascadedStyle;
+import org.xhtmlrenderer.css.style.CalculatedStyle;
+import org.xhtmlrenderer.css.style.EmptyStyle;
 import org.xhtmlrenderer.extend.ReplacedElement;
 import org.xhtmlrenderer.extend.ReplacedElementFactory;
 import org.xhtmlrenderer.extend.UserAgentCallback;
@@ -11,6 +15,8 @@ import org.xhtmlrenderer.layout.LayoutContext;
 import org.xhtmlrenderer.pdf.ITextFSImage;
 import org.xhtmlrenderer.pdf.ITextImageElement;
 import org.xhtmlrenderer.render.BlockBox;
+import org.xhtmlrenderer.render.Box;
+import org.xhtmlrenderer.render.RenderingContext;
 import org.xhtmlrenderer.simple.extend.FormSubmissionListener;
 
 import java.io.File;
@@ -51,10 +57,10 @@ class PsmlReplacedElementFactory implements ReplacedElementFactory {
     String nodeName = element.getNodeName();
 
     if ("image".equals(nodeName)) {
-      LOGGER.info("Replace {}", nodeName);
 
       // Local image
       String src = element.getAttribute("src");
+      LOGGER.debug("Replace {}: {}", nodeName, src);
       if (src.matches("^(?:[a-z0-9A-Z_-]{1,255})?(?:/[a-z0-9A-Z_-]{1,255}){1,16}\\.(?:png|jpg|gif)$")) {
         File f = new File(this.root, src);
         try {
@@ -62,8 +68,27 @@ class PsmlReplacedElementFactory implements ReplacedElementFactory {
           Image image = Image.getInstance(bytes);
           ITextFSImage fsImage = new ITextFSImage(image);
 
+          // Image dimensions (in pixels)
+          float imgWidthPx = image.getWidth();
+          float imgHeightPx = image.getHeight();
+
+          // Check available space on the page
+          Box masterBox = layoutContext.getLayer().getMaster();
+          float adjust = computeAdjustLayout(layoutContext, element);
+          float maxWidth = masterBox.getContentWidth() - adjust;
+          float maxHeight = (imgHeightPx * maxWidth) / imgWidthPx;
+
           if (cssWidth != -1 || cssHeight != -1) {
-            fsImage.scale(cssWidth, cssHeight);
+            // Scale image if necessary
+            if (cssWidth > maxWidth || cssHeight > maxHeight) {
+              float scale = maxWidth / cssWidth;
+              int targetWidth = Math.round(cssWidth * scale);
+              int targetHeight = Math.round(cssHeight * scale);
+              LOGGER.debug("Scale {}x{} to {}x{}", cssWidth, cssHeight, targetWidth, targetHeight);
+              fsImage.scale(targetWidth, targetHeight);
+            } else {
+              fsImage.scale(cssWidth, cssHeight);
+            }
           }
           return new ITextImageElement(fsImage);
 
@@ -90,6 +115,47 @@ class PsmlReplacedElementFactory implements ReplacedElementFactory {
   @Override
   public void setFormSubmissionListener(FormSubmissionListener listener) {
     this.superFactory.setFormSubmissionListener(listener);
+  }
+
+  /**
+   * Computes the adjustment value for the layout by accumulating the combined margins
+   * and paddings (left and right) of the specified element's ancestors.
+   *
+   * <p>The computation iterates through the element's parent hierarchy, retrieving and
+   * calculating the relevant CSS properties (margin-left, padding-left, margin-right,
+   * and padding-right) for each ancestor element, and summing them to determine the
+   * total adjustment value.
+   *
+   * @param layoutContext the current layout context, used to retrieve rendering and CSS information
+   * @param element the target element for which the layout adjustment is computed
+   *
+   * @return the computed adjustment value as a float, representing the cumulative effect of
+   *         margins and paddings across ancestor elements (in dots)
+   */
+  private float computeAdjustLayout(LayoutContext layoutContext, Element element) {
+    RenderingContext context = layoutContext.getSharedContext().newRenderingContextInstance();
+    float adjust = 0;
+
+    Element parent = (Element)element.getParentNode();
+    while (parent != null) {
+      CascadedStyle style = layoutContext.getCss().getCascadedStyle(parent, false);
+      if (style != null) {
+        CalculatedStyle cs = new EmptyStyle().deriveStyle(style);
+        float marginLeft = cs.getFloatPropertyProportionalTo(CSSName.MARGIN_LEFT, 0, context);
+        float paddingLeft = cs.getFloatPropertyProportionalTo(CSSName.PADDING_LEFT, 0, context);
+        float marginRight = cs.getFloatPropertyProportionalTo(CSSName.MARGIN_RIGHT, 0, context);
+        float paddingRight = cs.getFloatPropertyProportionalTo(CSSName.PADDING_RIGHT, 0, context);
+        adjust += (marginLeft+marginRight+paddingLeft+paddingRight);
+        LOGGER.debug("Parent: {} -> {}", parent.getNodeName(), adjust);
+      }
+
+      if (parent.getParentNode() instanceof Element) {
+        parent = (Element) parent.getParentNode();
+      } else {
+        parent = null;
+      }
+    }
+    return adjust;
   }
 
 }
